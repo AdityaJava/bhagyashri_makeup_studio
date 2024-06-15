e2438935036ceb66ec37863952adef7213ea02b2
Aditya <adityadudhal97@gmail.com>
2024-06-15 14:40:33 +0530

    displaying

--- mobile/script.js
+++ mobile/script.js
@@ -1,3 +1,9 @@
 let menuIcon = document.querySelector('#menu-icon');
 
 
+menuIcon.addEventListener('click', () => {
+    let mobileNavbar = document.querySelector('#mobile-sidebar');
+    console.log(mobileNavbar);
+    mobileNavbar.style.display = 'flex';
+
+});
