12f307a7f0d2be39a181825bce637d7d527b75bd
Aditya <adityadudhal97@gmail.com>
2024-06-15 14:52:51 +0530

    close sidebar

--- mobile/script.js
+++ mobile/script.js
@@ -5,5 +5,12 @@ menuIcon.addEventListener('click', () => {
     let mobileNavbar = document.querySelector('#mobile-sidebar');
     console.log(mobileNavbar);
     mobileNavbar.style.display = 'flex';
+});
 
+
+let crossBtn = document.querySelector('#close-sidebar');
+crossBtn.addEventListener('click', () => {
+    let mobileNavbar = document.querySelector('#mobile-sidebar');
+    console.log(mobileNavbar);
+    mobileNavbar.style.display = 'none';
 });
